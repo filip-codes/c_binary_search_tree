d3e47470100e1d222e30a2305489acdeb15a15b3
netbounty <filip.petrovic@netbounty.de>
2023-01-14 11:43:18 +0100

    finished task 1.2

--- tasks/two.c
+++ tasks/two.c
@@ -6,6 +6,8 @@
 #include "all.h"
 #include "../helpers/all.h"
 
+void add_to_array(struct Node *p_root, int **numbers, int *i);
+
 int cmpfunc (const void * a, const void * b) {
     return ( *(int*)a - *(int*)b );
 }
@@ -21,21 +23,40 @@ void execute_task_two() {
     if (numbers == NULL)
         return;
 
-    for (int i = 0; i < count; i++) {
-        numbers[i] = p_root->currentNumber;
-    }
+    int nmb = 0;
+
+    // Convert numbers from struct
+    add_to_array(p_root, &numbers, &nmb);
 
     qsort(numbers, count, sizeof(int), cmpfunc);
 
     for(int i = 0; i < count; i++)
         printf("%d ", numbers[i]);
 
-    /*printf("\n");
+    printf("\n");
     printf("Enter a number and search for it:\n");
     int number = 0;
     scanf("%d", &number);
-
-    hit_or_miss(p_root, number);*/
+    int *item = bsearch(&number, numbers, count, sizeof(int), cmpfunc);
+    if(item != NULL) {
+        printf("Hit = %d\n", *item);
+    } else {
+        printf("Item = %d could not be found\n", *item);
+    }
 
     // free noch schreiben
+}
+
+void add_to_array(struct Node *p_root, int **numbers, int *i) {
+    if (p_root == NULL || i == NULL)
+        return;
+
+    // Traverse root
+    (*numbers)[*i] = p_root->currentNumber;
+    (*i)++;
+
+    // Traverse left
+    add_to_array(p_root->leftNode, numbers, i);
+    // Traverse right
+    add_to_array(p_root->rightNode, numbers, i);
 }
